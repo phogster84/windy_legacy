7f51e2df84211fd49fd490da84c1ea7d2ee6a316
phogster84 <phil@phog.com>
2026-08-18 19:33:25 -0700

    Create pluginConfig.js

---
+++ pluginConfig.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'windy-plugin-rv-itinerary',
+  title: 'RV Trip Calendar Weather',
+  description: 'Loads an RV trip ICS calendar link and syncs the map, picker, and date timeline automatically.',
+  author: 'phogster84',
+  version: '1.1.0',
+  icon: '🚐',
+  repository: 'https://github.com/phogster84/windy',
+  desktopUI: 'rhpane',
+  mobileUI: 'small',
+  routerPath: '/rv-itinerary'
+};
